f6dad3e681a4d0d53aed9147e71e0947c5469794
John <jkiyak@gmail.com>
2021-07-25 17:38:44 -0500

    changing navbar to sidebar

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import {Card, CardColumns, Container} from 'react-bootstrap';
+
+
+
+function Landingpage(props) {
+return (
+<container-fluid>
+<h1>Hello</h1>
+<h2>testing</h2>
+</container-fluid>
+
+)
+}
+
+
+export default Landingpage;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,21 +1,51 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import { Nav } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
-import React from "react";
-import { Container } from 'react-bootstrap';
+import React, { Component } from "react";
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
 
 function Navbarcomponent (params) {
     return(
-      <Navbar bg="dark" variant="dark">
-      <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      </Container>
-    </Navbar>
+<SideNav
+    onSelect={(selected) => {
+        // Add your code here
+    }}
+>
+    <SideNav.Toggle />
+    <SideNav.Nav defaultSelected="home">
+    <h1>Picture here?</h1>
+        <NavItem eventKey="home">
+            <NavIcon>
+                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Home
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="charts">
+            <NavIcon>
+                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Charts
+            </NavText>
+            <NavItem eventKey="charts/linechart">
+                <NavText>
+                    Line Chart
+                </NavText>
+            </NavItem>
+            <NavItem eventKey="charts/barchart">
+                <NavText>
+                    Bar Chart
+                </NavText>
+            </NavItem>
+        </NavItem>
+    </SideNav.Nav>
+</SideNav>
+
+  
 
 
     )
